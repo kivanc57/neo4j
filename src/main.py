710779a48dc3d430c59bfa41fb02681f3ccd8d85
Kivanc Gordu <kivanc.gordu@hotmail.com>
2025-11-14 14:30:35 +0100

    feat: importing data @neo4j

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import pandas as pd
 from neo4j import GraphDatabase
 
 load_dotenv()
@@ -10,18 +11,33 @@ PASSWORD = os.getenv('NEO4J_PASSWORD')
 
 driver = GraphDatabase.driver(URI, auth=(USERNAME, PASSWORD))
 
-summary = driver.execute_query("""
-    CREATE (a:Person {name: $name})
-    CREATE (b:Person {name: $friendName})
-    CREATE (a)-[:KNOWS]->(b)
-    """,
-    name="Alice", friendName="David",
-    database_="neo4j",
-).summary
-print("Created {nodes_created} nodes in {time} ms.".format(
-    nodes_created=summary.counters.nodes_created,
-    time=summary.result_available_after
-))
+try:
+    driver.verify_connectivity()
+    print("Connection successful!")
+except Exception as e:
+    print(f"Failed to connect to Neo4j: {e}")
+
+excel_path = os.path.join("import", "sample.xlsx")
+df = pd.read_excel(excel_path)
+
+with driver.session(database="neo4j") as session:
+    for _, cols in df.iterrows():
+        query = """
+        CREATE (kdo:Kdo {name: $kdo_name})
+        -[t:CITED {format: $format, rok: $rok, číslo: $cislo, časopis: $casopis}]->
+        (koho:Koho {name: $koho_name})
+        """
+        session.run(
+            query,
+            parameters={
+                "kdo_name": cols["KDO"],
+                "koho_name": cols["KOHO"],
+                "format": int(cols["FORMAT"]),
+                "rok": int(cols["ROK"]),
+                "cislo": int(cols["ČÍSLO"]),
+                "casopis": cols["ČASOPIS"],
+            }
+        )
 
 driver.close()
 
